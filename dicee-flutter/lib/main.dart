ed211b21b2f2d3c0be0b80f57ed87feaf2cb2eef
ElmouradiAmine <elmouradi.amine98@gmail.com>
2020-05-01 14:59:47 +0000

    Finish dicee flutter app

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -15,9 +18,54 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed:
+              
+                  changeDice
+                
+              ,
+              child: Image.asset(
+                "images/dice$leftDiceNumber.png",
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: 
+                
+                  changeDice
+               
+              ,
+              child: Image.asset(
+                "images/dice$rightDiceNumber.png",
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void changeDice(){
+    setState(() {
+      rightDiceNumber = Random().nextInt(6)+1;
+      leftDiceNumber = Random().nextInt(6)+1;
+    });
+    
   }
 }
